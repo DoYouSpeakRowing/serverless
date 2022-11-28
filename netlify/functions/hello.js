22c08520f3359ec1515548d6f904fb189e2645b6
ehubin <edouard.hubin@gmail.com>
2022-11-29 00:11:12 +0100

    email!!!!

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,8 +1,23 @@
+import fetch from 'node-fetch'
 export const handler = async (event,context) => {
 	const eventBody = JSON.parse(event.body)
 	console.log(eventBody.postvar)
-	console.log({event})
-	console.log({context})
+	const body = {
+		sender: { name: "DoYouSpeakRowing",email:"doyouspeakrowing@gmail.com"},
+		to:[{email:process.env.TO_EMAIL}],
+		subject:"test sendmail",
+		htmlContent:"<html><head></head><body><p>Hello,</p>This is my first transactional email sent from Sendinblue.</p></body></html>"
+		};
+	const response = await fetch('https://api.sendinblue.com/v3/smtp/email',
+			{  	method: 'post',
+				body: JSON.stringify(body),
+				headers: {      'acccept': 'application/json',
+						'content-type':'application/json',
+						'api-key':process.env.SENDINBLUE_KEY
+					}
+			});
+	const data = await response.json();
+	console.log(data); }
 	return {
 		statusCode: 200,
 		headers : {'Access-Control-Allow-Origin': 'https://doyouspeakrowing.github.io'},
