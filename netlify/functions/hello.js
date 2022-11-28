d8e5a6157e2a16167d9f22851ff86ff442e7768a
ehubin <edouard.hubin@gmail.com>
2022-11-28 16:01:32 +0100

    add cors header

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,9 @@
 export const handler = async (event,context) => {
 	const eventBody = JSON.parse(event.body)
+	console.log(eventBody.postvar)
 	return {
 		statusCode: 200,
+		Access-Control-Allow-Origin: 'https://doyouspeakrowing.github.io/'
 		body: JSON.stringify({       message: 'Hello World!;'+process.env.TESTVAR+';'+eventBody.postvar,
 		}),
 	}
